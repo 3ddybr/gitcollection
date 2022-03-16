4180124c2c7869b4138949eac7f7659bf3a49a29
Alberto Consoli de Araujo <68253941+3ddybr@users.noreply.github.com>
2022-03-16 14:02:55 -0300

    estilizacao dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, Repos } from './styles';
+import logo from '../../assets/logo.svg';
 
 export const Dashboard: React.FC = () => {
   return (
     <>
-      <Title>Dashboard</Title>
+      <img src={logo} alt="GitCollection" />
+      <Title>Catálogo de repositórios do GitHub</Title>
+      <Form>
+        <input placeholder="username/repository_name" />
+        <button type="submit">Buscar</button>
+      </Form>
+      <Repos>
+        <a href="/repositories">
+          <img
+            src="https://avatars.githubusercontent.com/u/68253941?v=4"
+            alt=""
+          />
+          <div>
+            <strong>3ddybr/gitcolletion</strong>
+            <p>Repositório do curso de Reactjs</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repos>
     </>
   );
 };
